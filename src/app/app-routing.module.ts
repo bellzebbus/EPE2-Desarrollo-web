7fff334bbd19dbb3f5f71417236e1d477d40b8ad
sebastian arias <zebbas@outlook.com>
2020-10-20 09:23:24 -0300

    Contenidos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { Component, NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { ServicioComponent } from './servicio/servicio.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { JuegoComponent } from './juego/juego.component';
+
+
+const routes: Routes = [
+  {
+    path: "Home",
+    component:HomeComponent,
+  },
+  {
+    path: "portafolio",
+    component:PortafolioComponent,
+
+  },
+  {
+    path: "servicio",
+    component:ServicioComponent
+  },
+  {
+    path: "partner",
+    component:PartnersComponent
+  },
+  {
+    path: "contacto",
+    component:ContactoComponent
+  },
+  {
+    path: "juego",
+    component:JuegoComponent
+  },
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { ServicioComponent } from './servicio/servicio.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
+import { JuegoComponent } from './juego/juego.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarraLateralComponent,
+    HomeComponent,
+    PortafolioComponent,
+    ServicioComponent,
+    PartnersComponent,
+    ContactoComponent,
+    BoardComponent,
+    SquareComponent,
+    JuegoComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
